7e833db9b2458ed438258a038e233b0b7c686fcd
luanhenriqueee <cucoluan@gmail.com>
2024-10-01 11:05:01 -0300

    Update JavaApplication1.java

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -36,3 +36,24 @@ public void andar() {
      
 }
 }
+class Carro {
+  private String marca;
+  private String nome;
+  public Carro(String marca, String nome) {
+    this.marca = marca;
+    this.nome = nome;
+  }
+  protected String nomeCompleto() {
+    return marca + " " + nome;
+  }
+  public void andar() {
+    System.out.println("O carro " + nomeCompleto() + " está andando");
+  }
+}
+public class App {
+  public static void main(String[] args) {
+    Carro carro = new Carro("Fiat", "Gol");
+    carro.marca = "VW"; // Irá apresentar um erro de compilação
+    carro.andar();
+  }
+}
